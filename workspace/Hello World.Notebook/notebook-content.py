6f3f24d8a51c9232da118a22ba7a0e8faba3b248
ChantifiedLens <kchantnl@outlook.com>
2026-05-26 22:25:40 +0200

    initial commit

---
+++ workspace/Hello World.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "d5002b3f-9e0c-4bea-a0ed-9c6a67500f24",
+# META       "default_lakehouse_name": "WithoutSchema",
+# META       "default_lakehouse_workspace_id": "9ff8365a-dc53-41a4-a3eb-38ea65dca477",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "d5002b3f-9e0c-4bea-a0ed-9c6a67500f24"
+# META         }
+# META       ]
+# META     },
+# META     "environment": {
+# META       "environmentId": "a277ea4a-e87f-8537-4ce0-39db11d4aade",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+print("Hello World")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
